e8475e737ef30f86eacec2e053e389b0a9a9d0b4
ale-techdev <alejandro.techdev@gmail.com>
2026-02-15 15:09:50 -0500

    Add files via upload

---
+++ simplegram/template/config.py
@@ -0,0 +1,16 @@
+API_ID = None
+API_HASH = None
+BOT_TOKEN = None
+SESSION_NAME = "SIMPLEGRAM"
+
+ACCESS = False
+
+MESSAGES = {
+    "newAdmin": "Se le ha otorgado el estado de **administrador**",
+    "newOperator": "Se le ha otorgado el estado de **operador**",
+    "newUser": "Se le ha otorgado el estado de **usuario**",
+    "permissionDenied": "Usted no tiene permiso para acceder al sistema",
+    "noCommandPermission": "No tiene suficiente **acceso** para usar este comando",
+    "noCommandExists": "No existe ese comando"
+}
+
